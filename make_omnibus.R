daa64ebec4148201ff253083e7ceb6d0b6ea2867
Shanaa <shanaam@yorku.ca>
2021-12-02 16:23:27 -0500

    made omnibus function

---
+++ make_omnibus.R
@@ -0,0 +1,88 @@
+## --------------------------------
+##
+## Script name: makeOmnibus.R
+##
+## Purpose of script: Make omnibus dataframes (all trials, for all participants)
+##
+## Author: Shanaa Modchalingam
+##
+## Date created: 2021-21-02
+##
+## Email: s.modcha@gmail.com
+##
+## --------------------------------
+##
+##
+## --------------------------------
+
+## ----
+## Load packages
+library(data.table)
+library(tidyverse)
+
+## Functions
+# 
+
+# simple function for applying atan2 (removes baseline.. subtracts.. something)
+applyAtan2 <- function(df){
+  
+  # I should rotate target to 180 first. Test cases where it crosses 0, either direction
+  
+  return(((atan2(df[2] - df[4], df[1] - df[3]) * 180/pi) %% 360) - df[5]) # atan2(y,x) -- atan2 takes y first
+}
+
+# function to load 1 participant's data
+load_one_file <- function(path){
+  
+  # isolate the trial_result file
+  file_to_load <- list.files(path = path, 
+                             pattern = glob2rx("*trial_results.csv"), 
+                             full.names = TRUE)
+  
+  # load file
+  df <- fread(file_to_load, stringsAsFactors = FALSE)
+  
+  # remove large string to save space
+  df <- df %>%
+    select(experiment, ppid, trial_num, block_num,
+           trial_num_in_block, type, hand, score, 
+           starts_with("per_block"), starts_with("flick"),
+           starts_with("cursor"), starts_with("home"), 
+           starts_with("target"), error_size, magnitude)
+  
+  # return  df
+  return(df)
+}
+
+# function to load all files and merge
+make_omnibus <- function(){
+  
+  #build path
+  path <- "data/raw_data"
+  
+  # list to populate with dfs, and an iterator variable
+  list_dfs <- list()
+  i <- 1
+  
+  for (ppt_path in list.files(path = path, full.names = TRUE)){
+    # add session num to path
+    ppt_path <- paste(ppt_path, "S001", sep = '/')
+    
+    # add each df to the lsit
+    list_dfs[[i]] <- load_one_file(ppt_path)
+    
+    # increment i
+    i <- i+1
+  }
+  
+  # merge list
+  omnibus_df <- do.call(rbind, list_dfs)
+  
+  return(omnibus_df)
+}
+
+
+# test
+omnibus_df <- make_omnibus()
+# save the omnibus df
+fwrite(omnibus_df, file = "data/omnibus/omnibus_throws.csv")
