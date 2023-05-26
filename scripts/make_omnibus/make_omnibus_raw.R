a642b294e73cc5a70fcaa5effb7fc8980daf00be
Shanaa Modchalingam <s.modcha@gmail.com>
2023-05-26 15:51:51 -0400

    preprocessing scripts started

---
+++ scripts/make_omnibus/make_omnibus_raw.R
@@ -0,0 +1,233 @@
+## --------------------------------
+## Purpose of script: Make omnibus (per_trial) CSVs (no baseline correction)
+## Author: Shanaa Modchalingam
+##
+## Email: s.modcha@gmail.com
+##
+## --------------------------------
+##
+## Notes: uses data from data/preprocessed
+##
+## --------------------------------
+
+rm(list = ls()) # clean environment
+
+source("src/helper_funcs.R")
+
+library(data.table)
+library(tidyverse)
+library(future) # for parallel processing
+
+##### Variables #####
+to_load_dir_path <- "data/raw_data"
+to_save_dir_path <- "data/omnibus"
+original_exps <- c("rot15_cued_tilt", "rot15_uncued", "tilt_uncued_rot", 
+                   "tilt_uncued_norot", "tilt_cued_rot", "tilt_cued_norot")
+
+##### All Experiments #####
+
+make_omnibus_raw_file <- function(to_load_dir_path) {
+  ppt_list <- list.dirs(to_load_dir_path, recursive = FALSE)
+  # make a list of length length(ppt_list) 
+  trial_df_list <- vector("list", length(ppt_list))
+  
+  # loop through all directories in the to_load_dir_path
+  for (i in 1:length(ppt_list)) {
+    trial_df_list[[i]] <- future(make_one_ppt_file(i, ppt_list))
+  }
+  
+  # evaluate
+  future_output <- value(trial_df_list)
+  
+  # row bind all the trial_dfs
+  omnibus_df <- do.call(rbind, future_output)
+  
+  # save the omnibus_df
+  fwrite(omnibus_df, file = paste(to_save_dir_path, "omnibus_raw.csv", sep = "/"))
+}
+
+
+make_one_ppt_file <- function(directory_index, ppt_list) {
+  
+  ppt_dir <- ppt_list[directory_index]
+  
+  # print(ppt_dir) # debug 
+  
+  # load in all trial_results.csv files
+  trial_df <- fread(paste(ppt_dir, "S001/trial_results.csv", sep = "/"))
+  
+  ### remove things ###
+  ## ALL EXPS ## 
+  trial_df <- trial_df %>% 
+    filter(type != "instruction") %>%
+    select(-"session_num", -"indicator_angle", -"show_path", -"tilt_after_fire",
+           -"flick_multiplier")
+  
+  ## ORIGIAL EXPS ##
+  if (trial_df$experiment[1] %in% original_exps){
+    ### remove things ###
+    trial_df <- trial_df %>%
+      select(-starts_with("tracking_"), -starts_with("hand_"),
+             -starts_with("pinball_path"))
+    
+    ### add things ###
+    trial_df$anim_type <- "none"
+    trial_df$exp <- "original_exps"
+  } else if (trial_df$experiment[1] == "a_ball_roll_animate_surface"){
+    ## ANIMATE SURFACE EXP ##
+    ### remove things ###
+    trial_df <- trial_df %>%
+      select(-starts_with("hand_pos_flick"), -starts_with("ball_pos_step1"))
+    
+    ### add things ###
+    trial_df$exp <- "animate_surface"
+  }
+
+  trial_df <- trial_df %>% 
+    select(-"experiment")
+
+  
+  # # make a per trial summary and join it to the trial_df 
+  # hand_df_summary <- hand_df %>%
+  #   group_by(trial_num) %>%
+  #   summarise(
+  #     hand_time_start_move = first(time_start_move),
+  #     hand_time_reach = first(time_reach),
+  #     hand_time_total_move = first(time_total_move),
+  #     hand_angle_3cm_move = first(angle_3cm_move),
+  #   )
+  # 
+  # trial_df <- trial_df %>%
+  #   left_join(hand_df_summary, by = "trial_num")
+  
+  # return the trial_df
+  return(trial_df)
+}
+
+
+
+#####  Test ##### 
+# directory_index = 120
+# 
+# 
+# ppt_list <- list.dirs(to_load_dir_path, recursive = FALSE)
+# # make a list of length length(ppt_list) 
+# trial_df_list <- vector("list", length(ppt_list))
+# for (i in 1:length(ppt_list)) {
+#   trial_df_list[[i]] <- make_one_ppt_file(i, ppt_list)
+# }
+# 
+# omnibus_df <- do.call(rbind, trial_df_list)
+
+
+##### Do #####
+plan(multisession)
+make_omnibus_raw_file(to_load_dir_path)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# function to load all files and merge
+DEPRECATED <- function() {
+
+  # build path
+  path <- "data/raw_data"
+
+  # list to populate with dfs, and an iterator variable
+  list_dfs <- list()
+  i <- 1
+
+  for (ppt_path in list.files(path = path, full.names = TRUE)) {
+    # add session num to path
+    ppt_path <- paste(ppt_path, "S001", sep = "/")
+
+    # add each df to the lsit
+    list_dfs[[i]] <- load_one_file(ppt_path)
+
+    # increment i
+    i <- i + 1
+  }
+
+  # merge list
+  omnibus_df <- do.call(rbind, list_dfs)
+
+  # recode the initial 4 experiments
+  omnibus_df <- omnibus_df %>%
+    mutate(experiment = recode(
+      experiment,
+      "tilt_cued_norot" = "tilt_cued_tilt",
+      "tilt_uncued_norot" = "tilt_uncued",
+      "tilt_cued_rot" = "rot30_cued_tilt",
+      "tilt_uncued_rot" = "rot30_uncued",
+    ))
+
+  # replace any errors of 0.5 or more with NAs
+  omnibus_df <- omnibus_df %>%
+    mutate(error_size = replace(error_size, error_size > 0.70, NA))
+
+  ####
+  ####
+  #### IMPORTANT
+  # temporarily filter out ppt 81 and 82
+  omnibus_df <- omnibus_df %>%
+    filter(
+      ppid != 81, ppid != 82,
+      ppid != 83, ppid != 84
+    )
+
+  # add throw_angle
+  omnibus_df <- omnibus_df %>%
+    mutate(throw_angle = apply(omnibus_df[, c(
+      "flick_direction_x", "flick_direction_z",
+      "per_block_targetListToUse"
+    )],
+    1,
+    FUN = applyAtan2
+    ))
+
+  # fix trial numbers
+  omnibus_df$trial_num <- omnibus_df$trial_num - 213
+
+  # add a column for trial_set
+  omnibus_df <- omnibus_df %>%
+    mutate(trial_set = ifelse(
+      omnibus_df$trial_num %in% c(1, 2, 3, 4),
+      "init",
+      ifelse(omnibus_df$trial_num %in% c(77, 78, 79, 80),
+        "end",
+        ifelse(omnibus_df$trial_num %in% c(81, 82, 83, 84),
+          "washout_init",
+          ifelse(omnibus_df$trial_num %in% c(117, 118, 119, 120),
+            "washout_end",
+            ifelse(omnibus_df$trial_num %in% c(122, 123, 124, 125),
+              "transfer_init",
+              ifelse(omnibus_df$trial_num %in% c(158, 159, 160, 161),
+                "transfer_end", "other"
+              )
+            )
+          )
+        )
+      )
+    ))
+
+  # add throw_magnitude
+  omnibus_df <- omnibus_df %>%
+    mutate(throw_magnitude = sqrt(
+      flick_direction_x^2 +
+        flick_direction_y^2 +
+        flick_direction_z^2
+    ))
+
+  return(omnibus_df)
+}
--- src/helper_funcs.R
+++ src/helper_funcs.R
@@ -1,8 +1,7 @@
 library(psych) # for descriptives
 library(effsize) # for Cohen's d
 library(BayesFactor) # to compute Bayes factors
-library(tidyverse)
-library(data.table)
+
 
 # ----
 # Analysis
@@ -22,178 +21,86 @@ vector_confint <- function(vector, interval = 0.95) {
   return(error)
 }
 
+# atan2 using x and y
+atan2_2d <- function(x, y, target_ang) {
+  ang <- target_ang * -1 * pi / 180 # convert to rads
+  
+  x_r <- (x * cos(ang)) - (y * sin(ang))
+  y_r <- (x * sin(ang)) + (y * cos(ang))
+  
+  return(atan2(y_r, x_r) * 180 / pi) # atan2(y,x) -- atan2 takes y first
+}
+
+# bootstrapped confidence intervals
+# input = a vector
+bootstr_confint <- function(vector, interval = 0.95) {
+  # incomplete
+}
+
+# slope fitting function for the decay data
+decay_fit <- function(x_vec, y_vec) {
+  # Fit a line to the data
+  lm_fit <- lm(y_vec ~ x_vec)
+  
+  # Get the slope and intercept
+  coefs <- coef(lm_fit)
+  
+  # return the slope and intercept
+  return(coefs)
+}
+
+
+reg_confints <- function(x, y) {
+  n <- length(y) # Find length of y to use as sample size
+  lm.model <- lm(y ~ x) # Fit linear model
+  
+  # Extract fitted coefficients from model object
+  b0 <- lm.model$coefficients[1]
+  b1 <- lm.model$coefficients[2]
+  
+  # Find SSE and MSE
+  sse <- sum((y - lm.model$fitted.values)^2)
+  mse <- sse / (n - 2)
+  
+  t.val <- qt(0.995, n - 2) # Calculate critical t-value
+  
+  # Fit linear model with extracted coefficients
+  x_new <- 1:max(x)
+  y.fit <- b1 * x_new + b0
+  
+  # Find the standard error of the regression line
+  se <- sqrt(sum((y - y.fit)^2) / (n - 2)) * sqrt(1 / n + (x - mean(x))^2 / sum((x - mean(x))^2))
+  
+  # Fit a new linear model that extends past the given data points (for plotting)
+  # x_new2 <- 1:max(x + 100)
+  # y.fit2 <- b1 * x_new2 + b0
+  
+  # Warnings of mismatched lengths are suppressed
+  slope.upper <- suppressWarnings(y.fit + t.val * se)
+  slope.lower <- suppressWarnings(y.fit - t.val * se)
+  
+  # Collect the computed confidence bands into a data.frame and name the colums
+  bands <- data.frame(cbind(slope.lower, slope.upper))
+  colnames(bands) <- c("Lower Confidence Band", "Upper Confidence Band")
+  
+  # Plot the fitted linear regression line and the computed confidence bands
+  # plot(x, y, cex = 1.75, pch = 21, bg = 'gray')
+  # lines(y.fit, col = 'black', lwd = 2)
+  # lines(bands[1], col = 'blue', lty = 2, lwd = 2)
+  # lines(bands[2], col = 'blue', lty = 2, lwd = 2)
+  
+  return(bands)
+}
+
+
 # get the magnitude (euclidian normal) of a vector (this is faster than R's built in norm)
 norm_vec <- function(vector) {
   sqrt(crossprod(vector))
 }
 
-# atan2 using x, y, home_x, home_y, target(degrees)
-applyAtan2 <- function(df) {
-  x <- df[1]
-  y <- df[2]
-  ang <- df[3] * -1 * pi / 180 # convert to rads
-
-  x_r <- (x * cos(ang)) - (y * sin(ang))
-  y_r <- (x * sin(ang)) + (y * cos(ang))
-
-  return(atan2(y_r, x_r) * 180 / pi) # atan2(y,x) -- atan2 takes y first
-}
-
-
 # load data using fread
 loadData <- function(path) {
   data_df <- fread(path, stringsAsFactors = TRUE)
-
+  
   return(data_df)
-}
-
-# Plotting functions
-plot_desc_group_density <- function(df, group, y, subgroup = NULL, title = "Distributions within Groups") {
-  # plot some density things for visualizing data in groups
-  # group, and y are strings
-  if (is.null(subgroup)) {
-    p <- df %>%
-      ggplot(aes(.data[[group]], .data[[y]])) +
-      geom_violin(aes(fill = .data[[group]]), alpha = 0.2, draw_quantiles = c(.25, .5, .75), scale = "count") +
-      geom_beeswarm(alpha = 0.5) +
-      stat_summary(fun = mean, geom = "point", size = 3, color = "red")
-  } else {
-    p <- df %>%
-      ggplot(aes(.data[[group]], .data[[y]], colour = .data[[subgroup]])) +
-      geom_beeswarm(dodge.width = .9, alpha = 0.3) +
-      geom_violin(aes(fill = .data[[subgroup]]), alpha = 0.2, draw_quantiles = c(.25, .5, .75), scale = "count")
-  }
-
-  p <- p +
-    theme_minimal() +
-    theme(panel.grid.major.y = element_line(colour = "#CCCCCC")) +
-    ggtitle(title)
-
-  return(p)
-}
-
-plot_nice_group_density <- function(df, group, y, subgroup = NULL, title = "Distributions within Groups") {
-  # plot some density things for visualizing data in groups
-  # group, and y are strings
-  # plot some density things for visualizing data in groups
-  # group, and y are strings
-  group_tidy <- enquo(group)
-  y_tidy <- enquo(y)
-  subgroup_tidy <- enquo(subgroup)
-
-  desc_temp <- df %>%
-    group_by(!!group_tidy, !!subgroup_tidy) %>%
-    summarise(
-      means = mean(!!y_tidy),
-      sd = sd(!!y_tidy),
-      ci = vector_confint(!!y_tidy),
-      n = n(), .groups = "drop"
-    )
-
-  if (is.null(subgroup)) {
-    p <- df %>%
-      ggplot(aes(!!group_tidy, !!y_tidy, colour = !!group_tidy)) +
-      geom_beeswarm(alpha = 0.2) +
-      geom_linerange(
-        data = desc_temp,
-        aes(!!group_tidy, means,
-          ymin = means - ci, ymax = means + ci
-        ),
-        lwd = 5, alpha = 0.4,
-        position = position_dodge(width = .6)
-      ) +
-      geom_point(
-        data = desc_temp,
-        aes(y = means),
-        size = 5, alpha = 0.6,
-        position = position_dodge(width = .6)
-      )
-  } else {
-    p <- df %>%
-      ggplot(aes(!!group_tidy, !!y_tidy, colour = !!subgroup_tidy)) +
-      geom_beeswarm(dodge.width = .6, alpha = 0.2) +
-      geom_linerange(
-        data = desc_temp,
-        aes(!!group_tidy, means,
-          colour = !!subgroup_tidy,
-          ymin = means - ci, ymax = means + ci
-        ),
-        lwd = 5, alpha = 0.4,
-        position = position_dodge(width = .6)
-      )
-  }
-
-  p <- p +
-    theme_minimal() +
-    theme(panel.grid.major.y = element_line(colour = "#CCCCCC")) +
-    ggtitle(title)
-
-  return(p)
-}
-
-
-# ----
-# from paper (find)
-bayes_t_test <- function(df, group_title, group1, group2, dv) {
-  # bayes t-test
-
-  data <- df %>%
-    group_by(NULL) %>%
-    filter(.data[[group_title]] == group1 | .data[[group_title]] == group2) %>%
-    select(group_title, dv)
-
-  res.bayes <- data.frame(matrix(NA, nrow = 1, ncol = 4))
-  rownames(res.bayes) <- colnames(data)[-1]
-  colnames(res.bayes) <- c("BF01", "Postd.Median", "Postd.LB", "Postd.UB")
-
-  # the test
-
-  i <- 1 # this is useful for iterating
-  # Note: code from https://osf.io/gfdjy/
-  # Removing NAs
-  data.noNA <- data[!is.na(data[, i + 1]), ]
-
-  # Identify independent groups,
-  # BF01:
-  tmp <- ttestBF(pull(filter(data.noNA, .data[[group_title]] == group1)[, dv]),
-    pull(filter(data.noNA, .data[[group_title]] == group2)[, dv]),
-    rscale = .707
-  )
-  BF01 <- 1 / extractBF(tmp)$bf
-
-  # Posterior distribution for BF01:
-  set.seed(i)
-  tmp <- ttestBF(pull(filter(data.noNA, .data[[group_title]] == group1)[, dv]),
-    pull(filter(data.noNA, .data[[group_title]] == group2)[, dv]),
-    posterior = TRUE,
-    iterations = 10000,
-    rscale = .707
-  )
-  CI.bayes <- quantile(tmp[, "delta"], probs = c(.03, .97))
-
-  res.bayes[i, ] <- c(BF01, median(tmp[, "delta"]), CI.bayes)
-
-  # show
-  res.bayes
-
-  #    2.6 Posterior model probabilities for equal prior odds ----
-  # post.H0:
-  null <- res.bayes$BF01 / (1 + res.bayes$BF01)
-  # post.H1:
-  alt <- 1 / (1 + res.bayes$BF01)
-
-  return(sprintf("BF01: %.3f, P(D|H0): %.3f, P(D|H1): %.3f, Groups: %s vs %s", res.bayes$BF01, null, alt, group1, group2))
-}
-
-##### MODIFIED FUNCTIONS FROM SMCL PACKAGE
-
-convert_cell_to_numvec <- function(v) {
-  # split by commas:
-  v <- strsplit(v, "_")
-  # convert to numeric:
-  v <- lapply(v, FUN = as.numeric)
-  # make vector:
-  v <- as.vector(unlist(v))
-
-  return(v)
 }
